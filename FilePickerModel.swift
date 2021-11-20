a94cf34f2ca9efb4770522b52b4b74493e4a28db
Hanlin Mai <hanlinm@bu.edu>
2021-11-20 14:13:20 -0500

    Update

---
+++ FilePickerModel.swift
@@ -0,0 +1,22 @@
+//
+//  FilePickerModel.swift
+//  SwingOn by EC463 Team 20
+//  Created on 11/17/21.
+//
+
+/*
+import SwiftUI
+
+struct FilePickerModel: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<FilePickerModel>) -> UIDocumentPickerViewController {
+        let filePicker = UIDocumentPickerViewController(documentTypes: [], in: .open)
+        filePicker.allowsMultipleSelection = false
+        return filePicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: UIViewControllerRepresentableContext<FilePickerModel>) {
+    }
+    
+}
+*/
