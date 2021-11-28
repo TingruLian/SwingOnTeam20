82626f0be01dd6156339969667fb2297e52d57d9
Jessica Martinez Marquez <jessmtzm@bu.edu>
2021-11-28 16:55:52 -0500

    Video feature and UI connection

--- CameraModel.swift
+++ CameraModel.swift
@@ -3,18 +3,104 @@
 //  SwingOn
 //
 //  Created by Jessica Martinez Marquez on 11/20/21.
+// Video
 //
 
+
+//import SwiftUI
+import AVFoundation
 import SwiftUI
 
-struct CameraModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class CameraModel: ObservableObject{
+    
+    // Create capture session
+    var cameraFeedSession = AVCaptureSession()
+    //var delegate: AVCapturePhotoCaptureDelegate?
+    
+    let output = AVCaptureVideoDataOutput()
+    //var previewLayer: AVCaptureVideoPreviewLayer?
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    func checkPermissions(){
+        switch AVCaptureDevice.authorizationStatus(for: .video){
+            
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) {[weak self] granted in guard granted else {return}
+                // set up camera
+                DispatchQueue.main.async {
+                    self?.cameraSetUp()
+                }
+            }
+        case .restricted:
+            break
+        case .denied:
+            break
+        case .authorized:
+            cameraSetUp()
+            return
+        @unknown default:
+            break
+        }
+    }
+    
+    private func cameraSetUp(){
+        //let session = AVCaptureSession()
+        
+        // Choose device --> rear camera
+        let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+        
+        // Use device as input
+        guard
+            let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!), cameraFeedSession.canAddInput(videoDeviceInput)
+        else{return}
+        cameraFeedSession.addInput(videoDeviceInput)
+         
+        // Add output
+        if cameraFeedSession.canAddOutput(output){ 
+            cameraFeedSession.addOutput(output)
+        }
+        
+//        // Set preview layer
+//        previewLayer.videoGravity = .resizeAspectFill
+//        previewLayer.session = session
+        
+        // Run capture session to start receiving video frames
+//        session.startRunning()
+//        cameraFeedSession = session
     }
 }
 
-struct CameraModel_Previews: PreviewProvider {
-    static var previews: some View {
-        CameraModel()
+// To preview video on VideoViewer.swift
+struct CameraPreview: UIViewRepresentable{
+    var camera: CameraModel
+    
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView(frame: UIScreen.main.bounds)
+        
+        camera.previewLayer = AVCaptureVideoPreviewLayer(session: camera.cameraFeedSession)
+        camera.previewLayer.frame = view.frame
+        camera.previewLayer?.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(camera.previewLayer)
+        
+        // Run capture session to start receiving video frames
+        camera.cameraFeedSession.startRunning()
+        
+        return view
     }
+    func updateUIView(_ uiView: UIViewType, context: Context) {}
+    
 }
+
+
+
+//struct CameraModel: View {
+//    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//    }
+//}
+//
+//struct CameraModel_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CameraModel()
+//    }
+//}
--- VideoViewer.swift
+++ VideoViewer.swift
@@ -4,12 +4,26 @@
 //
 //  Created by Jessica Martinez Marquez on 11/20/21.
 //
+// Screen to visualize video
+//
 
 import SwiftUI
 
 struct VideoViewer: View {
+    @StateObject var camera = CameraModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            ZStack{
+                // Camera preview area
+                CameraPreview(camera: camera)
+                    .ignoresSafeArea(.all,edges: .all)
+            }
+            .onAppear(perform: {
+                camera.checkPermissions()
+            })
+        }
+        //.navigationBarTitle(<#T##SwiftUI.Text#>)
     }
 }
 
