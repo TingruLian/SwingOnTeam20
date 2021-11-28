d1f30977ca8f36b80d87de9736ce20e7e3723fbc
Jessica Martinez Marquez <jessmtzm@bu.edu>
2021-11-28 16:48:12 -0500

    Video

---
+++ VideoViewer.swift
@@ -0,0 +1,20 @@
+//
+//  VideoViewer.swift
+//  SwingOn
+//
+//  Created by Jessica Martinez Marquez on 11/20/21.
+//
+
+import SwiftUI
+
+struct VideoViewer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoViewer_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoViewer()
+    }
+}
