072ffb2aaec8aa27138883bed9b9c1fc942ab696
Jessica Martinez Marquez <jessmtzm@bu.edu>
2021-11-28 16:48:12 -0500

    Video

---
+++ CameraModel.swift
@@ -0,0 +1,20 @@
+//
+//  CameraModel.swift
+//  SwingOn
+//
+//  Created by Jessica Martinez Marquez on 11/20/21.
+//
+
+import SwiftUI
+
+struct CameraModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CameraModel_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraModel()
+    }
+}
