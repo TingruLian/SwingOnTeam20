adccfb2faf438cc92a3ce0c261acfb6c589ec1aa
Jessica Martinez Marquez <jessmtzm@bu.edu>
2022-04-21 15:22:33 -0400

    add player detected banner

--- VideoViewer.swift
+++ VideoViewer.swift
@@ -11,20 +11,107 @@ import SwiftUI
 
 struct VideoViewer: View {
     @StateObject var camera = CameraModel()
+//    @ObservedObject
+    var player = Predictor().playerDetected
+    
+    
+//    @State var show = false // needs to change to observed object so that message pop ups when there is a player
+    
+  
+    
     var body: some View {
-        //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         NavigationView{
             ZStack{
-                // Camera preview area
+// MARK: Camera preview area
                 CameraPreview(camera: camera)
                     .ignoresSafeArea(.all,edges: .all)
-            }
-            .onAppear(perform: {
-                camera.checkPermissions()
-            })
-        }
+                
+//                Color.white
+//                    .ignoresSafeArea(.all,edges: .all)
+//                    .navigationBarHidden(true)
+                
+                // For testing notice
+                
+            
+//                VStack{
+//                Button("Show"){
+//                    withAnimation{
+//                        show.toggle()
+//                    }
+                //}
+                let _ = print("vv player: \(player)")
+                if !player {
+                    VStack{
+                        Image(systemName: "checkmark")
+                            .foregroundColor(.white)
+                            .font(.system(size: 48, weight: .regular))
+                            .padding(EdgeInsets(top: 20, leading: 5, bottom: 5, trailing: 5))
+                        Text("Player Detected")
+                            .foregroundColor(.white)
+                            .font(.callout)
+                            .padding(EdgeInsets(top: 0, leading: 10, bottom: 5, trailing: 10))
+                    }
+                    .background(Color.gray.opacity(0.75))
+                    .cornerRadius(5)
+//
+////                    Rectangle()
+////                        .fill(.red)
+////                        .frame(width: 200, height: 200)
+////                        .transition(.opacity)
+                    .onAppear{
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            withAnimation(.easeOut){
+//                              show.toggle()
+                            }
+                        }
+                    }
+                } // if show
+//                else { // feedback "no player detected"
+////                    Feeback.speak(_phrase: "Upload from Photos")
+//
+//                }
+                    
+//                } //VStack
+                
+//                RoundedRectangle(cornerRadius: 30)
+//                    .fill(Color.blue)
+//                    .padding()
+//                    .animation(.easeOut(duration: 2).delay(0.5), value: show)
+//                    .onAppear {
+//                        show.toggle()
+//                    }
+                    
+                
+                
+                
+//                VStack{
+//                    Spacer()
+//
+//                    Button(action: {
+//                        print("Taking Picture")
+//                    }, label: {
+//                        ZStack{
+//                            Circle().frame(width: 65, height: 65)
+//                                .foregroundColor(.orange)
+//                            Circle().stroke(Color.white, lineWidth: 4)
+//                                .frame(width: 75, height: 75)
+//                        }
+//                    })
+//                } //VStack
+            } // ZStack
+//            .onTapGesture {
+//                       withAnimation(.spring()) {
+//                           show.toggle()
+//                       }
+//            }
+            .onAppear(perform: { camera.checkPermissions() })
+            .onDisappear(perform: { camera.closeCamera() })
+             
+            
         //.navigationBarTitle(<#T##SwiftUI.Text#>)
     }
+        
+}
 }
 
 struct VideoViewer_Previews: PreviewProvider {
